c44a8321983e3910d60866e1665a48b5ea94b1b3
Monu <monupoonia746@example.com>
2025-10-07 14:24:52 +0530

    first commit

---
+++ glowcompare/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './index.css'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <div className="text-5xl text-red-500 font-bold underline">
+  Tailwind Fixed 💃
+</div>
+
+    </>
+  )
+}
+
+export default App
